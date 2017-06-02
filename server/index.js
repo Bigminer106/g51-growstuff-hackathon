400581e587969f722e86ff5f4a03c887342b5631
Adam Miner <miner.adam1@hotmail.com>
2017-06-02 14:33:58 -0600

    Changes Made

--- server/index.js
+++ server/index.js
@@ -7,32 +7,42 @@ app.use(cors());
 
 const growStuff = [{
   name: "tomato",
-  lifeCycle: [2,3],
-  tempMinMax: [68,80],
+  image: 'images/tomato.png',
+  lifeCycle: 3,
+  tempMin: 68,
+  tempMax: 80,
   nutrientRatio: [5,10,10],
   climate: "temperate"
 }, {
   name: "potato",
-  lifeCycle: [2,4],
-  tempMinMax: [50,80],
+  image: 'images/potato.png',
+  lifeCycle: 4,
+  tempMin: 50,
+  tempMax: 80,
   nutrientRatio: [6,3,8],
   climate: "any"
 }, {
   name: "carrot",
-  lifeCycle: [2,3],
-  tempMinMax: [30,80],
+  image: 'images/carrot.png',
+  lifeCycle: 3,
+  tempMin: 30,
+  tempMax: 80,
   nutrientRatio: [5,15,15],
   climate: "temperate"
 }, {
   name: "corn",
-  lifeCycle: [2,3],
-  tempMinMax: [40,85],
+  image: 'images/corn.png',
+  lifeCycle: 3,
+  tempMin: 40,
+  tempMax: 85,
   nutrientRatio: [7,9,6],
   climate: "any"
 }, {
   name: "onion",
-  lifeCycle: [3,6],
-  tempMinMax: [40,80],
+  image: 'images/onion.png',
+  lifeCycle: 6,
+  tempMin:40,
+  tempMax: 80,
   nutrientRatio: [3,4,3],
   climate: "temperate"
 }];
